55f7c1fc76e28e9f75814d0b0078bdc80888e475
Simon Hanna <simon.h4nn4@gmail.com>
2016-04-16 16:06:34 +0200

    Improve selection of next submission to grade

--- iliasCorrector/views.py
+++ iliasCorrector/views.py
@@ -19,13 +19,21 @@ def exercise(exercise_id):
     return render_template('exercise.html', exercise=exercise, submissions=submissions)
 
 
+def get_next_submission(exercise_id, ident=''):
+    exercise = Exercise.query.filter_by(id=exercise_id).first()
+    return exercise.submissions.filter_by(grade=None).join(Student).order_by(
+            'student.ident').filter(Student.ident > ident).first()
+
+
 @app.route('/exercise/<exercise_id>/submission/')
 @app.route('/exercise/<exercise_id>/submission/<submission_id>/', methods=['GET', 'POST'])
 def submission(exercise_id=None, submission_id=None):
     if not submission_id:
-        submission = Submission.query.filter_by(exercise_id=exercise_id, grade=None).join(Student).order_by('student.ident').first()
+        submission = get_next_submission(exercise_id)
         return redirect(url_for('submission', exercise_id=exercise_id, submission_id=submission.id))
     submission = Submission.query.get_or_404(submission_id)
+    next_submission = get_next_submission(exercise_id,
+                                          submission.student.ident)
     if request.method == 'POST':
         grade = request.form.get('grade', None)
         remarks = request.form.get('remarks', None)
@@ -34,12 +42,12 @@ def submission(exercise_id=None, submission_id=None):
         submission.remarks = remarks.replace(';', '-')
         db.session.add(submission)
         db.session.commit()
-        next_submission = submission.exercise.submissions.filter_by(grade=None).join(Student).order_by('student.ident').first()
         if not next_submission:
             flash('Finished correcting submissions for exercise {}'.format(submission.exercise), 'success')
             return redirect(url_for('exercise', exercise_id=exercise_id))
         return redirect(url_for('submission', exercise_id=exercise_id, submission_id=next_submission.id))
-    return render_template('submission.html', submission=submission)
+    return render_template('submission.html', submission=submission,
+                           next_submission=next_submission)
 
 
 @app.route('/files/<file_id>/')
