0c496e3404138fecee2614f44925c4dcd6ab053f
Simon Hanna <simon.h4nn4@gmail.com>
2017-05-03 17:57:45 +0200

    Move BASE_DIR one directory up

--- iliasCorrector/__init__.py
+++ iliasCorrector/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 
